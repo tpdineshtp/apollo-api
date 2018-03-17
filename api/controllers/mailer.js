5cbfdfa576f2a63a4d3589fb0c80d0c89dc8f61f
Dinesh Palanisamy <Dinesh.Palanisamy@Xome.com>
2018-03-18 00:32:42 +0530

    [Modifications on Request info mailer]

--- api/controllers/mailer.js
+++ api/controllers/mailer.js
@@ -11,17 +11,17 @@ exports.send_mail = function(req, res) {
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'tpdineshtp@gmail.com',
+      user: 'myinfowebsite.resume@gmail.com',
       pass: 'Dinesh&05'
     }
   });
 
-  var text = "Name : "+req.body.name+", Email : "+req.body.email+", Number : "+req.body.number+", Message : "+req.body.message;
+  var text = "Name : "+req.body.Your_Name+", Email : "+req.body.Your_Email+", Number : "+req.body.Phone_Number+", Message : "+req.body.Message;
 
   var mailOptions = {
-    from: 'tpdinesh@gmail.com',
+    from: 'myinfowebsite.resume@gmail.com',
     to: 'tpdineshtp@gmail.com',
-    subject: 'Hire me request My Info!',
+    subject: 'Hire me request from My Info!',
     text: text
   };
 
@@ -29,7 +29,7 @@ exports.send_mail = function(req, res) {
     if (error) {
       res.status(500).send();
     } else {
-      res.status(200).send(info.response);
+      res.status(200).send();
     }
   });
 };
